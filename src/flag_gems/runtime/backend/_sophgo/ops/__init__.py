43513ea7810331f9015b96fdcae621565eb0ea14
yi.pan <yi.pan@sophgo.com>
2026-05-15 16:28:50 +0800

    [sophgo] simplify scatter mul kernel and support int32 index
    - Replace atomic_cas loop with direct store in scatter mul kernel
    - Use int32 index for scatter tests on sophgo backend

--- src/flag_gems/runtime/backend/_sophgo/ops/__init__.py
+++ src/flag_gems/runtime/backend/_sophgo/ops/__init__.py
@@ -53,6 +53,7 @@ from .randn import randn
 from .randn_like import randn_like
 from .randperm import randperm
 from .rms_norm import rms_norm
+from .scatter import scatter, scatter_
 from .select_scatter import select_scatter
 from .sigmoid import sigmoid
 from .slice_scatter import slice_scatter
@@ -134,6 +135,8 @@ __all__ = [
     "randn_like",
     "randperm",
     "rms_norm",
+    "scatter",
+    "scatter_",
     "sigmoid",
     "select_scatter",
     "slice_scatter",
